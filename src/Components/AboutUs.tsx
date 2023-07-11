819adf0aa2825211c77f92f695f7d3059d9a751f
Komal <waghmarekomal21@gmail.com>
2023-07-11 11:39:19 +0530

    Local circle

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 function AboutUs() {
@@ -40,7 +39,7 @@ function AboutUs() {
           
           </h2>
           <p className="text-black-800 font-serif text-2xl">
-          Whether you're a local vendor seeking to expand your reach and grow your customer base or a discerning customer eager to explore the richness of your neighbourhood, LocalCircle is your answer. Join our growing community of local enthusiasts and experience the power of meaningful connections, economic empowerment, and a stronger, more vibrant local economy.
+            Whether you're a local vendor seeking to expand your reach and grow your customer base or a discerning customer eager to explore the richness of your neighbourhood, LocalCircle is your answer. Join our growing community of local enthusiasts and experience the power of meaningful connections, economic empowerment, and a stronger, more vibrant local economy.
           </p>
           <a
             href="#"
@@ -56,62 +55,3 @@ function AboutUs() {
 }
 
 export default AboutUs;
-=======
-import React from 'react';
-
-function AboutUs() {
-  return (
-    <div id="About" className="grid grid-cols-1 md:grid-cols-2 gap-6 flex flex-col space-y-1 px-8">
-      <div className="py-12 px-8 mt-12 mx-8">
-        <h2 className="font-serif mb-4 text-purple-800">
-          <p className="text-7xl font-bold py-2">LocalCircle</p>
-          <p className="text-4xl font-bold py-2">Promoting Local Prosperity</p>
-          <p className="text-4xl font-bold text-black py-2">Unlock Your Vicinity's Potential</p>
-        </h2>
-        <p className="text-black-800 font-serif text-2xl">
-          Welcome to LocalCircle, the revolutionary solution that is reshaping the way local vendors and customers connect.
-          We have developed an innovative IoT and digital platform designed to empower economic growth by helping local
-          businesses flourish and enabling customers to discover and enjoy the unique offerings in their vicinity.
-        </p>
-        <a
-          href="#"
-          className="mt-4 inline-flex items-center justify-center bg-yellow-400  text-black font-serif font-bold py-2 px-4 rounded"
-          style={{ height: '10%' }}
-        >
-          <span style={{ fontSize: '20px' }}>Coming Soon...</span>
-        </a>
-      </div>
-      <div className="py-8 p-8 flex justify-center">
-        <img src="src/assets/about.png" alt="About image" className="w-96" />
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-6 flex flex-col space-y-1 px-8 py-12">
-        <div className="py-8 p-8 flex -rotate-12 justify-center">
-          <img src="src/assets/about2.svg" alt="About image" className="w-50" />
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-6 px-8">
-        <div className="py-12 mt-12 mx-8">
-          <h2 className="font-serif mb-4 text-blue-800">
-            <p className="text-4xl font-bold py-2">Elevate Your Local Shopping Experience: LocalCircle Brings Customers and Vendors Together</p>
-          
-          </h2>
-          <p className="text-black-800 font-serif text-2xl">
-          Whether you're a local vendor seeking to expand your reach and grow your customer base or a discerning customer eager to explore the richness of your neighbourhood, LocalCircle is your answer. Join our growing community of local enthusiasts and experience the power of meaningful connections, economic empowerment, and a stronger, more vibrant local economy.
-          </p>
-          <a
-            href="#"
-            className="mt-4 inline-flex items-center justify-center bg-yellow-400  text-black font-serif font-bold py-2 px-4 rounded"
-            style={{ height: '10%' }}
-          >
-            <span style={{ fontSize: '20px' }}>Coming Soon...</span>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default AboutUs;
->>>>>>> 33766db0efb8c2c55b050c6ce4a5fe58b0bf3199
